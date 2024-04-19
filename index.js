c656a9031cde864dc4179d25169a1c4022183d39
yousra40 <yusraalbaghdady123456@gmail.com>
2024-04-20 01:28:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "dol"];
+
+function writeCards(arr, event) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const msg = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+    newArray.push(msg);
+  }
+  return newArray;
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function wrapGifts(gifts) {
+  let i = 0;
+  while (i < gifts.length) {
+    console.log(`Wrapped ${gifts[i]} and added a bow`);
+    i++;
+  }
+  return gifts;
+}
+wrapGifts(gifts);
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num--);
+  }
+}
+countDown(10);
